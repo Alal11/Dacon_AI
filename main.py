c3cb70fc5933c7298b6b3f01fc0e3788dd5be4bf
Alal11 <spalqj56@naver.com>
2024-07-17 22:37:43 +0900

    모델 훈련, AUC 점수 계산, 모델 검증 함수 정의

--- main.py
+++ main.py
@@ -136,3 +136,82 @@ class MLP(nn.Module):
         x = torch.sigmoid(x)
         return x
     
+
+
+from sklearn.metrics import roc_auc_score
+
+# 모델을 훈련하는 주요 함수
+def train(model, optimizer, train_loader, val_loader, device):
+    model.to(device)
+    criterion = nn.BCELoss().to(device)
+    
+    best_val_score = 0
+    best_model = None
+    
+    for epoch in range(1, CONFIG.N_EPOCHS+1):
+        model.train()
+        train_loss = []
+        for features, labels in tqdm(iter(train_loader)):
+            features = features.float().to(device)
+            labels = labels.float().to(device)
+            
+            optimizer.zero_grad()
+            
+            output = model(features)
+            loss = criterion(output, labels)
+            
+            loss.backward()
+            optimizer.step()
+            
+            train_loss.append(loss.item())
+                    
+        _val_loss, _val_score = validation(model, criterion, val_loader, device)
+        _train_loss = np.mean(train_loss)
+        print(f'Epoch [{epoch}], Train Loss : [{_train_loss:.5f}] Val Loss : [{_val_loss:.5f}] Val AUC : [{_val_score:.5f}]')
+            
+        if best_val_score < _val_score:
+            best_val_score = _val_score
+            best_model = model
+    
+    return best_model
+
+# 각 클래스에 대해 AUC 점수의 평균을 계산하여 반환하는 함수
+def multiLabel_AUC(y_true, y_scores):
+    auc_scores = []
+    for i in range(y_true.shape[1]):
+        auc = roc_auc_score(y_true[:, i], y_scores[:, i])
+        auc_scores.append(auc)
+    mean_auc_score = np.mean(auc_scores)
+    return mean_auc_score
+
+# 모델의 검증 과정을 처리하는 함수
+def validation(model, criterion, val_loader, device):
+    model.eval()
+    val_loss, all_labels, all_probs = [], [], []
+    
+    with torch.no_grad():
+        for features, labels in tqdm(iter(val_loader)):
+            features = features.float().to(device)
+            labels = labels.float().to(device)
+            
+            probs = model(features)
+            
+            loss = criterion(probs, labels)
+
+            val_loss.append(loss.item())
+
+            all_labels.append(labels.cpu().numpy())
+            all_probs.append(probs.cpu().numpy())
+        
+        _val_loss = np.mean(val_loss)
+
+        all_labels = np.concatenate(all_labels, axis=0)
+        all_probs = np.concatenate(all_probs, axis=0)
+        
+        # Calculate AUC score
+        auc_score = multiLabel_AUC(all_labels, all_probs)
+    
+    return _val_loss, auc_score
+
+
+
